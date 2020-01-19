1caea05aa2af58b2ef2eae730cf151863df8e02c
Justin Brown <justinbrown.work@gmail.com>
2020-01-19 11:08:16 -0600

    Initialize amplify auth and api with Apollo client.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Amplify, { Auth } from "aws-amplify";
+import { withAuthenticator } from "aws-amplify-react";
+import AWSAppSyncClient from "aws-appsync";
+import awsconfig from "./aws-exports";
+import { ApolloProvider } from "@apollo/react-hooks";
+import logo from "./logo.svg";
+import "./App.css";
+
+Amplify.configure(awsconfig);
+
+const client = new AWSAppSyncClient({
+  url: awsconfig.aws_appsync_graphqlEndpoint,
+  region: awsconfig.aws_appsync_region,
+  auth: {
+    type: awsconfig.aws_appsync_authenticationType,
+    jwtToken: async () =>
+      (await Auth.currentSession()).getIdToken().getJwtToken()
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ApolloProvider>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
